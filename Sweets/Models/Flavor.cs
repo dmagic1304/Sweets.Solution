67a3809346b9e85842f104514d39eeaedeb5a427
Dominik Magic <dominikmagic5@gmail.com>
2023-01-13 09:17:46 -0800

    Flavor  model added

---
+++ Sweets/Models/Flavor.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sweets.Models
+{
+  public class Flavor
+  {
+    public int FlavorId { get; set; }
+
+    public string Taste { get; set; }    
+
+  }
+}
